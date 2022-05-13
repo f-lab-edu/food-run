4b4cd65298237807e368c30ed00439bae82721ee
dailyzett <dailyzett@gmail.com>
2022-05-13 17:32:10 +0900

    프로젝트 생성

---
+++ src/test/java/com/flab/foodsubscription/FoodsubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.foodsubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
