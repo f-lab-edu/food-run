0ed3dfdbb12d1a49dc925f6bc6413c0cb51ce1fb
dailyzett <dailyzett@gmail.com>
2022-06-07 00:43:51 +0900

    컨트롤러가 서비스 의존하도록 변경
    UserMapper > UserService

--- src/main/java/com/flab/foodrun/web/user/UserController.java
+++ src/main/java/com/flab/foodrun/web/user/UserController.java
@@ -1,28 +1,32 @@
 package com.flab.foodrun.web.user;
 
-import com.flab.foodrun.domain.user.dao.UserMapper;
+import com.flab.foodrun.domain.user.service.UserService;
+import com.flab.foodrun.web.user.form.UserSaveForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
 @RequestMapping("/users")
+@RestController
 @RequiredArgsConstructor
 public class UserController {
 
-	private final UserMapper userMapper;
-
-	@GetMapping("/add")
-	public String addForm(){
-		return "GET:addForm";
-	}
+	private final UserService userService;
 
 	@PostMapping("/add")
-	public String addUser(){
-		return "POST:addUser";
+	public Object addUser(@Validated @ModelAttribute("user") UserSaveForm form,
+		BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			log.info("error={}", bindingResult);
+			return bindingResult.getAllErrors();
+		}
+		return userService.addUser(form);
 	}
 }
