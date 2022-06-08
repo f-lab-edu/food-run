2c377480f8a06d95db071c786d9e0d1f26a17ac1
dailyzett <dailyzett@gmail.com>
2022-06-09 00:09:28 +0900

    매퍼 로직 추가
    - findLoginIdById: 아이디로 로그인 아이디를 찾음

--- src/main/java/com/flab/foodrun/domain/user/dao/UserMapper.java
+++ src/main/java/com/flab/foodrun/domain/user/dao/UserMapper.java
@@ -3,8 +3,13 @@ package com.flab.foodrun.domain.user.dao;
 import com.flab.foodrun.domain.user.User;
 import org.apache.ibatis.annotations.Mapper;
 
+//MyBatis @Mapper 애노테이션
 @Mapper
 public interface UserMapper {
+
 	int insertUser(User user);
+
 	int countByLoginId(String loginId);
+
+	String findLoginIdById(Long id);
 }
