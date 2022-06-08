db853370a0fb4e4d2830ce60291d42deec6dc816
dailyzett <dailyzett@gmail.com>
2022-06-09 00:08:31 +0900

    UserService 인터페이서 제거 후 통합

--- src/main/java/com/flab/foodrun/domain/user/service/UserService.java
+++ src/main/java/com/flab/foodrun/domain/user/service/UserService.java
@@ -1,8 +1,35 @@
 package com.flab.foodrun.domain.user.service;
 
 import com.flab.foodrun.domain.user.User;
+import com.flab.foodrun.domain.user.dao.UserMapper;
 import com.flab.foodrun.web.user.form.UserSaveForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-	User addUser(UserSaveForm form);
+//로그 출력 애노테이션
+@Slf4j
+@Service
+//초기화 되지 않는 final 필드에 대해 자동으로 생성자를 만들어주는 애노테이션
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserMapper userMapper;
+	private final PasswordEncoder passwordEncoder;
+
+	public User addUser(UserSaveForm userSaveForm) {
+		validateDuplicatedUser(userSaveForm.getLoginId());
+		userSaveForm.setPassword(passwordEncoder.encode(userSaveForm.getPassword()));
+		User user = userSaveForm.toEntity();
+		userMapper.insertUser(user);
+		return user;
+	}
+
+	private void validateDuplicatedUser(String loginId) {
+		int loginIdCount = userMapper.countByLoginId(loginId);
+		if (loginIdCount > 0) {
+			throw new IllegalStateException("이미 존재하는 회원입니다");
+		}
+	}
 }
