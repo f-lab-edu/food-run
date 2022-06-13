396dd4e8e44efa1ce8e03d1b04f351c5cb03dde3
dailyzett <dailyzett@gmail.com>
2022-06-13 12:38:28 +0900

    주석 변경 및 @Data 애노테이션 삭제

--- src/main/java/com/flab/foodrun/domain/user/User.java
+++ src/main/java/com/flab/foodrun/domain/user/User.java
@@ -2,17 +2,17 @@ package com.flab.foodrun.domain.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-//@Getter, @Setter, @RequiredArgsConstructor,@ToString, @EqualsAndHashCode, @Value를 합친 애노테이션
-@Data
-//Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
-@Builder
-//인자가 없는 생성자를 만들어줌
-@NoArgsConstructor
-//모든 인자가 존재하는 생성자를 만들어줌
-@AllArgsConstructor
+@Getter // getter 메서드 자동 생성 애노테이션
+@Setter // setter 메서드 자동 생성 애노테이션
+@ToString // toString() 메서드 자동 생성 애노테이션
+@Builder //Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
+@NoArgsConstructor //인자가 없는 생성자를 만들어줌
+@AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어줌
 public class User {
 
 	private Long id;
