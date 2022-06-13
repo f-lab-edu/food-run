c08b69fa1ad2b6ae1adfc1d3d3779a029971fa1c
dailyzett <dailyzett@gmail.com>
2022-06-13 12:32:04 +0900

    Bean validation 오류 메시지 설정 추가

--- src/main/java/com/flab/foodrun/web/user/form/UserSaveForm.java
+++ src/main/java/com/flab/foodrun/web/user/form/UserSaveForm.java
@@ -10,40 +10,39 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-//Getter, Setter 메서드를 만들어줌.
-@Getter
-@Setter
-//Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
-@Builder
-//모든 인자가 존재하는 생성자를 만들어줌
-@AllArgsConstructor
+
+@Getter // getter 메서드 자동 생성 애노테이션
+@Setter // setter 메서드 자동 생성 애노테이션
+@Builder //Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
+@AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어줌
 public class UserSaveForm {
 
-	@NotBlank
+	@NotBlank(message = "{loginId.notBlank}")
 	private String loginId;
 
-	@NotBlank
+	@NotBlank(message = "{password.notBlank}")
 	private String password;
 
-	@NotBlank
+	@NotBlank(message = "{name.notBlank}")
 	private String name;
 
-	@NotBlank
+	@NotBlank(message = "{role.notBlank}")
 	private String role;
 
-	@NotBlank
+	@NotBlank(message = "{status.notBlank}")
 	private String status;
 
-	@NotBlank
+	@NotBlank(message = "{phoneNumber.notBlank}")
 	private String phoneNumber;
 
 	@Email
+	@NotBlank(message = "{email.notBlank}")
 	private String email;
 
-	@NotBlank
+	@NotBlank(message = "{streetAddress.notBlank}")
 	private String streetAddress;
 
-	@NotBlank
+	@NotBlank(message = "{detailAddress.notBlank}")
 	private String detailAddress;
 
 	public User toEntity() {
