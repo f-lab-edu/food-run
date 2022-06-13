444c8e354fd52d76c2a313c69699176cc3b5e002
dailyzett <dailyzett@gmail.com>
2022-06-13 12:31:37 +0900

    BindingResult 삭제 및 주석 추가

--- src/main/java/com/flab/foodrun/web/user/UserController.java
+++ src/main/java/com/flab/foodrun/web/user/UserController.java
@@ -1,9 +1,13 @@
 package com.flab.foodrun.web.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.flab.foodrun.domain.user.User;
 import com.flab.foodrun.domain.user.service.UserService;
 import com.flab.foodrun.web.user.form.UserSaveForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,23 +15,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 로깅에 대한 추상 레이어를 제공하는 인터페이스 모음 애노테이션 인터페이스를 사용하면 나중에 로깅 라이브러리를 변경해도 코드의 변경 없이 실행 가능
+ */
 @Slf4j
 @RequestMapping("/users")
-//@Controller, @ResponseBody 가 합쳐진 애노테이션
-@RestController
-@RequiredArgsConstructor
+@RestController //@Controller + @ResponseBody
+@RequiredArgsConstructor // final 필드에 대해 자동으로 생성자를 만들어주는 애노테이션
 public class UserController {
 
 	private final UserService userService;
 
 	@PostMapping("/signup")
-	public Object addUser(@Validated @ModelAttribute("user") UserSaveForm form,
-		BindingResult bindingResult) {
-
-		if (bindingResult.hasErrors()) {
-			log.info("error={}", bindingResult);
-			return bindingResult.getAllErrors();
-		}
-		return userService.addUser(form);
+	public ResponseEntity<User> addUser(@Validated @ModelAttribute UserSaveForm form) {
+		return new ResponseEntity<>(userService.addUser(form), HttpStatus.CREATED);
 	}
 }
