1a2c23f53831c3903a64369951bbf43572b4753a
dailyzett <dailyzett@gmail.com>
2022-06-13 21:14:15 +0900

    주석 내용 변경

--- src/main/java/com/flab/foodrun/domain/user/dao/UserMapper.java
+++ src/main/java/com/flab/foodrun/domain/user/dao/UserMapper.java
@@ -3,7 +3,10 @@ package com.flab.foodrun.domain.user.dao;
 import com.flab.foodrun.domain.user.User;
 import org.apache.ibatis.annotations.Mapper;
 
-//MyBatis @Mapper 애노테이션
+/**
+ * MyBatis는 최소 하나의 SqlSessionFactory와 Mapper Interface가 필요
+ * Mapper Interface 를 만들어주는 애노테이션
+ */
 @Mapper
 public interface UserMapper {
 
