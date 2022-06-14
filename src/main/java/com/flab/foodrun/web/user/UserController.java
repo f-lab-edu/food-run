6ea5c1847bde6c8112840eea51b08171dc639891
dailyzett <dailyzett@gmail.com>
2022-06-15 02:44:15 +0900

    JSON 객체를 받기 위해 @ModelAttribute -> @RequestBody 변경

--- src/main/java/com/flab/foodrun/web/user/UserController.java
+++ src/main/java/com/flab/foodrun/web/user/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping
-	public ResponseEntity<User> addUser(@Validated @ModelAttribute UserSaveForm form) {
+	public ResponseEntity<User> addUser(@Validated @RequestBody UserSaveForm form) {
 		return new ResponseEntity<>(userService.addUser(form), HttpStatus.CREATED);
 	}
 }
