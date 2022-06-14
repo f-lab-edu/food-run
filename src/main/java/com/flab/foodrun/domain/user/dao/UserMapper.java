f1cf4331e3d0e3fb4ccb6109f74223b1a7d24192
dailyzett <dailyzett@gmail.com>
2022-06-15 02:10:52 +0900

    selectUserByLoginId 인터페이스 메서드 추가

--- src/main/java/com/flab/foodrun/domain/user/dao/UserMapper.java
+++ src/main/java/com/flab/foodrun/domain/user/dao/UserMapper.java
@@ -1,11 +1,11 @@
 package com.flab.foodrun.domain.user.dao;
 
 import com.flab.foodrun.domain.user.User;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
- * MyBatis는 최소 하나의 SqlSessionFactory와 Mapper Interface가 필요
- * Mapper Interface 를 만들어주는 애노테이션
+ * MyBatis는 최소 하나의 SqlSessionFactory와 Mapper Interface가 필요 Mapper Interface 를 만들어주는 애노테이션
  */
 @Mapper
 public interface UserMapper {
@@ -13,4 +13,6 @@ public interface UserMapper {
 	int insertUser(User user);
 
 	int countByLoginId(String loginId);
+
+	Optional<User> selectUserByLoginId(int id);
 }
