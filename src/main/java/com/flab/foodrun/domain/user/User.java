1a7ccf557a482932b7594d3c0f1e1d7bf8ef43b3
dailyzett <dailyzett@gmail.com>
2022-06-16 06:27:30 +0900

    @ToString 제거

--- src/main/java/com/flab/foodrun/domain/user/User.java
+++ src/main/java/com/flab/foodrun/domain/user/User.java
@@ -5,11 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter // getter 메서드 자동 생성 애노테이션
 @Setter // setter 메서드 자동 생성 애노테이션
-@ToString // toString() 메서드 자동 생성 애노테이션
 @Builder //Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
 @NoArgsConstructor //인자가 없는 생성자를 만들어줌
 @AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어줌
