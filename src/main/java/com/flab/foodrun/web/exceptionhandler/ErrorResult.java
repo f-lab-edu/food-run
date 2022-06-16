87a29d9cc683fed6e7ea65e762cd5668f49d649f
dailyzett <dailyzett@gmail.com>
2022-06-17 01:27:25 +0900

    각 필드 private 으로 선언

--- src/main/java/com/flab/foodrun/web/exceptionhandler/ErrorResult.java
+++ src/main/java/com/flab/foodrun/web/exceptionhandler/ErrorResult.java
@@ -4,6 +4,6 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어줌
 public class ErrorResult {
-	public String code;
-	public String message;
+	private String code;
+	private String message;
 }
