919497a4f71eab95eedd941d4a3e0837a273f6e5
dailyzett <dailyzett@gmail.com>
2022-06-17 01:44:08 +0900

    각 필드 private 으로 선언하고 @Getter 추가

--- src/main/java/com/flab/foodrun/web/exceptionhandler/ErrorResult.java
+++ src/main/java/com/flab/foodrun/web/exceptionhandler/ErrorResult.java
@@ -1,7 +1,9 @@
 package com.flab.foodrun.web.exceptionhandler;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어줌
 public class ErrorResult {
 	private String code;
