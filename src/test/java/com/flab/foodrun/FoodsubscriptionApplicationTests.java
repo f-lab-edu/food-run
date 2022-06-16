8fd868dba3868ff709cc337ffde7ded4746cb13f
dailyzett <dailyzett@gmail.com>
2022-06-17 01:25:41 +0900

    스프링부트 진입점 테스트 코드 추가

--- src/test/java/com/flab/foodrun/FoodsubscriptionApplicationTests.java
+++ src/test/java/com/flab/foodrun/FoodsubscriptionApplicationTests.java
@@ -1,13 +1,27 @@
 package com.flab.foodrun;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.flab.foodrun.domain.user.service.UserService;
+import com.flab.foodrun.web.user.UserController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FoodsubscriptionApplicationTests {
 
+	@Autowired
+	UserController userController;
+
+	@Autowired
+	UserService userService;
+
 	@Test
 	void contextLoads() {
+		assertThat(userController).isNotNull();
+		assertThat(userService).isNotNull();
 	}
 
 }
