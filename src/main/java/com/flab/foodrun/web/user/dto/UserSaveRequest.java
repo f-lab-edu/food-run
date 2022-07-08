5f6ec3b42a2f7bdfa5e2c85c7cf1c547f13afa3c
dailyzett <dailyzett@gmail.com>
2022-07-08 22:32:33 +0900

    필요없는 롬복 제거

--- src/main/java/com/flab/foodrun/web/user/dto/UserSaveRequest.java
+++ src/main/java/com/flab/foodrun/web/user/dto/UserSaveRequest.java
@@ -6,15 +6,11 @@ import com.flab.foodrun.domain.user.UserStatus;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter // getter 메서드 자동 생성 애노테이션
-@Setter // setter 메서드 자동 생성 애노테이션
 @Builder //Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
-@AllArgsConstructor // 모든 인자가 존재하는 생성자를 만들어줌
 public class UserSaveRequest {
 
 	@NotBlank(message = "{loginId.notBlank}")
