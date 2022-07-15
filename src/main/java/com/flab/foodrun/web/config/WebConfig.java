38f07db9dbac4ee603fdf997684a7890742bc2e5
dailyzett <dailyzett@gmail.com>
2022-07-15 21:35:38 +0900

    인터셉터 제외 목록에 "/users" URI는 제외

--- src/main/java/com/flab/foodrun/web/config/WebConfig.java
+++ src/main/java/com/flab/foodrun/web/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addInterceptor(new LoginInterceptor())
 			.order(1)
 			.addPathPatterns("/**")
-			.excludePathPatterns("/users", "/login");
+			.excludePathPatterns("/login");
 	}
 }
