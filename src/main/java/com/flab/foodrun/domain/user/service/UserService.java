6f1b77512c6904064f3ac6e5fc427c285ab40a58
dailyzett <dailyzett@gmail.com>
2022-07-22 20:19:18 +0900

    메서드 인자 변경
    - User 도메인 -> UserDto 객체

--- src/main/java/com/flab/foodrun/domain/user/service/UserService.java
+++ src/main/java/com/flab/foodrun/domain/user/service/UserService.java
@@ -4,10 +4,13 @@ import com.flab.foodrun.domain.login.exception.LoginIdNotFoundException;
 import com.flab.foodrun.domain.user.User;
 import com.flab.foodrun.domain.user.dao.UserMapper;
 import com.flab.foodrun.domain.user.exception.DuplicatedUserIdException;
+import com.flab.foodrun.web.user.dto.UserModifyRequest;
+import com.flab.foodrun.web.user.dto.UserSaveRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 로깅에 대한 추상 레이어를 제공하는 인터페이스 모음 애노테이션 인터페이스를 사용하면 나중에 로깅 라이브러리를 변경해도 코드의 변경 없이 실행 가능
@@ -15,16 +18,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor // final 필드에 대해 자동으로 생성자를 만들어주는 애노테이션
+@Transactional
 public class UserService {
 
 	private final UserMapper userMapper;
 	private final PasswordEncoder passwordEncoder;
 
-	public User addUser(User user) {
-		if (userMapper.countByLoginId(user.getLoginId()) > 0) {
+	public User addUser(UserSaveRequest userSaveRequest) {
+		if (userMapper.countByLoginId(userSaveRequest.getLoginId()) > 0) {
 			throw new DuplicatedUserIdException();
 		}
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		userSaveRequest.setPassword(passwordEncoder.encode(userSaveRequest.getPassword()));
+		User user = userSaveRequest.toEntity();
+		log.info("addUser:{}", user);
 		userMapper.insertUser(user);
 		return user;
 	}
@@ -34,9 +40,12 @@ public class UserService {
 			.orElseThrow(LoginIdNotFoundException::new);
 	}
 
-	public User modifyUser(User user) {
+	public User modifyUser(UserModifyRequest userModifyRequest) {
+		User user = findUser(userModifyRequest.getLoginId());
+		user.setUser(userModifyRequest.getName(), userModifyRequest.getEmail(),
+			userModifyRequest.getPhoneNumber());
+
 		userMapper.updateUser(user);
-		log.info("update info:{}, {}, {}", user.getId(), user.getPassword(), user.getEmail());
 		return user;
 	}
 }
