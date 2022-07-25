4930271b96bf8f679d8115e9de050b7a8f225f3b
dailyzett <dailyzett@gmail.com>
2022-07-26 02:34:13 +0900

    findUser 메서드는 읽기 전용 트랜잭션 적용

--- src/main/java/com/flab/foodrun/domain/user/service/UserService.java
+++ src/main/java/com/flab/foodrun/domain/user/service/UserService.java
@@ -13,8 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 로깅에 대한 추상 레이어를 제공하는 인터페이스 모음 애노테이션 인터페이스를 사용하면 나중에 로깅 라이브러리를 변경해도 코드의 변경 없이 실행 가능
+ * @Slf4j: 로깅에 대한 추상 레이어를 제공하는 인터페이스 모음 애노테이션 인터페이스를 사용하면 나중에 로깅 라이브러리를 변경해도 코드의 변경 없이 실행 가능
+ * @Transactional : 적용된 범위에서 트랜잭션 기능이 포함된 프록시 객체가 생성, 자동으로 커밋 혹은 롤백 진행
  */
+
 @Slf4j
 @Service
 @RequiredArgsConstructor // final 필드에 대해 자동으로 생성자를 만들어주는 애노테이션
@@ -35,6 +37,7 @@ public class UserService {
 		return user;
 	}
 
+	@Transactional(readOnly = true)
 	public User findUser(String loginId) {
 		return userMapper.selectUserByLoginId(loginId)
 			.orElseThrow(LoginIdNotFoundException::new);
