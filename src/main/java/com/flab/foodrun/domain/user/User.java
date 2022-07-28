8bae01c013a8c183765798ccb3a1a0813eb547e8
dailyzett <dailyzett@gmail.com>
2022-07-29 03:52:43 +0900

    User 엔티티 내부에 UserAddress 엔티티 필드 추가

--- src/main/java/com/flab/foodrun/domain/user/User.java
+++ src/main/java/com/flab/foodrun/domain/user/User.java
@@ -9,11 +9,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter // getter 메서드 자동 생성 애노테이션
-@Setter // setter 메서드 자동 생성 애노테이션
-@Builder //Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
-@NoArgsConstructor //인자가 없는 생성자를 만들어줌
-@AllArgsConstructor //모든 인자가 존재하는 생성자를 만들어줌
+/**
+ * @Getter getter 메서드 자동 생성 애노테이션
+ * @Setter setter 메서드 자동 생성 애노테이션
+ * @Builder Builder 패턴을 쉽게 사용할 수 있게 도와주는 애노테이션
+ * @NoArgsConstructor 인자가 없는 생성자를 만들어준다
+ * @AllArgsConstructor 모든 인자가 존재하는 생성자를 만들어준다
+ * @ToString 모든 객체에 의해 상속된 toString 메서드에 대한 구현을 생성한다.
+ */
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class User {
 
@@ -25,8 +34,7 @@ public class User {
 	private UserStatus status;
 	private String phoneNumber;
 	private String email;
-	private String streetAddress;
-	private String detailAddress;
+	private UserAddress userAddress;
 
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private LocalDateTime createdAt;
