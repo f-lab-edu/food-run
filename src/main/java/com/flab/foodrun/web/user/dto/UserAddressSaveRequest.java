bf749329fd238b66b4e622733f68d0929cd4e235
dailyzett <dailyzett@gmail.com>
2022-07-30 01:15:32 +0900

    사용하지 않는 메서드 제거

--- src/main/java/com/flab/foodrun/web/user/dto/UserAddressSaveRequest.java
+++ src/main/java/com/flab/foodrun/web/user/dto/UserAddressSaveRequest.java
@@ -1,18 +1,15 @@
 package com.flab.foodrun.web.user.dto;
 
-import com.flab.foodrun.domain.user.UserAddress;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@ToString
 public class UserAddressSaveRequest {
 
 	@NotNull
@@ -21,10 +18,4 @@ public class UserAddressSaveRequest {
 	@NotNull
 	private String detailAddress;
 
-	public UserAddress toEntity() {
-		return UserAddress.builder()
-			.streetAddress(streetAddress)
-			.detailAddress(detailAddress)
-			.build();
-	}
 }
