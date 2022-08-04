5db53f2d5c1b42a291349a698e8c54aa41474d66
dailyzett <dailyzett@gmail.com>
2022-08-05 00:49:54 +0900

    @EnableRedisHttpSession 제거. 환경 종속적이지 않은 Embedded Redis 를 사용하기 위함.

--- src/main/java/com/flab/foodrun/FoodRunApplication.java
+++ src/main/java/com/flab/foodrun/FoodRunApplication.java
@@ -2,14 +2,12 @@ package com.flab.foodrun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * @EnableRedisHttpSession SessionRepository 필터를 SpringSessionRepositoryFilter 라는 이름의 빈으로 등록
  */
 
 @SpringBootApplication
-@EnableRedisHttpSession
 public class FoodRunApplication {
 
 	public static void main(String[] args) {
