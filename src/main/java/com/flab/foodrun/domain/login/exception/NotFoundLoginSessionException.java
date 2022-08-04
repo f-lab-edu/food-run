ad3f184d9276283e5b822b85643d7261fe9c7aaf
dailyzett <dailyzett@gmail.com>
2022-08-05 08:48:12 +0900

    로그인 세션 찾을 수 없을 때 던지는 예외 추가

---
+++ src/main/java/com/flab/foodrun/domain/login/exception/NotFoundLoginSessionException.java
@@ -0,0 +1,8 @@
+package com.flab.foodrun.domain.login.exception;
+
+public class NotFoundLoginSessionException extends RuntimeException {
+
+	public NotFoundLoginSessionException() {
+		super();
+	}
+}
