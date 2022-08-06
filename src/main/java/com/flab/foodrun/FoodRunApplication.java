8372f5137a50c34b71fafdddfb2377ac2bb74f88
dailyzett <dailyzett@gmail.com>
2022-08-06 13:42:18 +0900

    필요없는 주석 삭제

--- src/main/java/com/flab/foodrun/FoodRunApplication.java
+++ src/main/java/com/flab/foodrun/FoodRunApplication.java
@@ -3,10 +3,6 @@ package com.flab.foodrun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @EnableRedisHttpSession SessionRepository 필터를 SpringSessionRepositoryFilter 라는 이름의 빈으로 등록
- */
-
 @SpringBootApplication
 public class FoodRunApplication {
 
